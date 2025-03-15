48a1d95929d3dfc564a514c95008e15beaf839d0
yaminhossen <streatdot616@gmail.com>
2025-03-15 22:43:03 +0600

    asset problem solve

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -5,12 +5,12 @@ import { request } from 'http';
 // const fs = require('node:fs');
 module.exports = async function (fastify: FastifyInstance) {
     fastify
-        // .get('/', async (_req: FastifyRequest, reply: FastifyReply) => {
-        //     // return reply.view('website/index.ejs');
-        //     console.log('root');
-        //     return reply.send('You got it this is the root or home page');
-        //     // return reply.redirect('/admin');
-        // })
+        .get('/', async (_req: FastifyRequest, reply: FastifyReply) => {
+            // return reply.view('website/index.ejs');
+            console.log('root');
+            return reply.send('You got it this is the root or home page');
+            // return reply.redirect('/admin');
+        })
         .get(
             '/student',
             // { preHandler: auth_middleware },
